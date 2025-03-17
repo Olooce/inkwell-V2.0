0351ea71df4a7eff8da76b837dffae5a760de9bd
Olooce <121952433+Olooce@users.noreply.github.com>
2025-03-17 17:34:04 +0300

    deps: Update 2 file(s) in ./Update 1 file(s) in views/Update 1 file(s) in src/
    modified: ./package-lock.json
    modified: ./package.json
    modified: src/main.js
    modified: views/CreateStory.vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { userStore } from './stores/userStore'
+import Vue3Toastify from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+
+const Toast = Vue3Toastify;
 
 // Load stored user data if available
 userStore.loadStoredUser()
@@ -24,6 +28,7 @@ const vuetify = createVuetify({
 const app = createApp(App)
 app.use(vuetify)
 app.use(router)
+app.use(Toast)
 app.mount('#app')
 
 
