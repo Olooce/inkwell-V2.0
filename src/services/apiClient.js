321c9dfb9f6d0e876cf1fea44cfe1c5b172867cd
Olooce <121952433+Olooce@users.noreply.github.com>
2025-03-19 00:51:04 +0300

    chore: Update services/apiClient.js, views/Comics.vue
    modified: services/apiClient.js
    modified: views/Comics.vue

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'vue3-toastify'
 
-const BASE_URL = import.meta.env.MODE === 'development' && import.meta.env.VITE_DEV_BASE_URL
+export const BASE_URL = import.meta.env.MODE === 'development' && import.meta.env.VITE_DEV_BASE_URL
   ? import.meta.env.VITE_DEV_BASE_URL
   : (typeof window !== 'undefined' ? `${window.location.protocol}//${window.location.host}/api` : '/api');
 
